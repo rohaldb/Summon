6415c6c40decd8ac78edebaa4d0208f4f6899a4d
rohaldbUni <z5019999@ad.unsw.edu.au>
2020-04-10 15:01:42 +1000

    Initial Commit

---
+++ Summon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Summon
+//
+//  Created by Benjamin Rohald on 10/4/20.
+//  Copyright © 2020 Benjamin Rohald. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Summon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Summon
+//
+//  Created by Benjamin Rohald on 10/4/20.
+//  Copyright © 2020 Benjamin Rohald. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
